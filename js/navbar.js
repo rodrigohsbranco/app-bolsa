98f528a7bad5e28424e3f46fadde1b33604de8b7
Rodrigo Branco <rodrigohsbranco@gmail.com>
2024-01-12 00:12:27 -0300

    iniciado página com navbar

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+var tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+var tabPanels = document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex) {
+    tabButtons.forEach(function (node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabButtons[panelIndex].style.backgroundColor = "white";
+    tabPanels.forEach(function (node) {
+        node.style.display = "none";
+    });
+    tabPanels[panelIndex].style.display = "flex";
+}
+showPanel(0);
